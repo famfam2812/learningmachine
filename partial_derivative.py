be00c40bae1ea76d4f6a7c0475a461c528f26c61
BuildTools <unconfigured@null.spigotmc.org>
2020-08-31 14:00:51 +0200

    added partial_derivative

--- partial_derivative.py
+++ partial_derivative.py
@@ -1,62 +1,129 @@
-import matplotlib
-import nnfs
 import numpy as np
+# We have 3 samples (feature sets) of data now
+inputs = np.array([[1, 2, 3, 2.5],
+ [2., 5., -1., 2],
+ [-1.5, 2.7, 3.3, -0.8]])
+# We have 3 sets of weights - one set for each neuron now
+weights = np.array([[0.2, 0.8, -0.5, 1],
+ [0.5, -0.91, 0.26, -0.5],
+ [-0.26, -0.27, 0.17, 0.87]]).T
+# One bias for each neuron
+biases = np.array([[2], [3], [0.5]]).T
 
-#forward pass
-x = [1.0, -2.0, 3.0] #input
-w = [-3.0, -1.0, 2.0] #weights
-b = 1.0 # bias
+# Forward pass
+layer_outputs = np.dot(inputs, weights) + biases # Dense layer
+relu_outputs = np.maximum(0, layer_outputs) # ReLU activation
+# Let's optimize and test backpropagation here
+# ReLU activation - simulates derivative with respect to input values
+# from next layer passed to current layer during backpropagation
+relu_dinputs = np.ones(relu_outputs.shape)
+relu_dinputs[layer_outputs <= 0] = 0
+drelu = relu_dinputs
 
-#multiplying inputs by weights
-wx0 = x[0] * w[0]
-wx1 = x[1] * w[1]
-wx2 = x[2] * w[2]
+# Dense layer
+# dinputs - multiply by weights
+dinputs = np.dot(drelu, weights.T)
+# dweights - multiply by inputs
+dweights = np.dot(inputs.T, drelu)
 
-#adding
-s = wx0 + wx1 + wx2 + b
+# dbiases - sum values, do this over samples (first axis), keepdims
+# since this by default will produce a plain list -
+# we explained this in the chapter 4
+dbiases = np.sum(drelu, axis=0, keepdims=True)
 
+# Update parameters
+weights += -0.001 * dweights
+biases += -0.001 * dbiases
 
-#ReLU activation function
-y = max(s, 0)
-print(y)
+print(weights)
+print(biases)
 
+# Dense layer
+class Layer_Dense:
+    # Layer initialization
+    def __init__(self, inputs, neurons):
+        self.weights = 0.01 * np.random.randn(inputs, neurons)
+        self.biases = np.zeros((1, neurons))
+    # Forward pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-dy = (1 if s > 0 else 0)
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
-dwx0 = 1 * dy
-dwx1 = 1 * dy
-dwx2 = 1 * dy
-db = 1 * dy
+# ReLU activation
+class Activation_ReLU:
+    # Forward pass
+    def forward(self, inputs):
+        # Remember input values
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
 
-dx0 = w[0] * dwx0
-dw0 = x[0] * dwx0
+    # Backward pass
+    def backward(self, dvalues):
+        # Since we need to modify the original variable,
+        # let's make a copy of the values first
+        self.dinputs = dvalues.copy()
+        # Zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
 
-dx1 = w[1] * dwx1
-dw1 = x[1] * dwx1
+class Loss:
 
-dx2 = w[2] * dwx2
-dw2 = x[2] * dwx2
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
 
-dx = [dx0, dx1, dx2] # gradients on inputs
-dw = [dw0, dw1, dw2] # gradients on weights
-db # gradient on bias...just 1 bias here.
+        data_loss = np.mean(sample_losses)
 
-print(db, dx0, dw0, dx1, dw1, dx2, dw2)
+        return data_loss
 
-w[0] += -0.001 * dw[0]
-w[1] += -0.001 * dw[1]
-w[2] += -0.001 * dw[2]
-b += -0.001 * db
+class Loss_CategoricalCrossentropy(Loss):
 
-print(w, b)
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred) #number of samples
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7) #clip data for at undgå divison af 0
+        y_pred_clipped = y_pred_clipped[range(samples), y_true] #sandsynlighed for target value
+        negative_log_likelihoods = -np.log(y_pred_clipped) #losses
+        return negative_log_likelihoods
 
-# Multiplying inputs by weights
-wx0 = x[0] * w[0]
-wx1 = x[1] * w[1]
-wx2 = x[2] * w[2]
+    # Backward pass
+    def backward(self, dvalues, y_true):
+        # Number of samples
+        samples = len(dvalues)
+        # Number of labels in every sample
+        # We'll use the first sample to count them
+        labels = len(dvalues[0])
+        # If labels are sparse, turn them into one-hot vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+            # Calculate gradient
+            self.dinputs = -y_true / dvalues
+            # Normalize gradient
+            self.dinputs = self.dinputs / samples
 
-# Adding
-s = wx0 + wx1 + wx2 + b
-# ReLU activation function
-y = max(s, 0)
-print(y)
+class Activation_Softmax:
+    def forward(self, inputs):
+
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+    # Backward pass
+    def backward(self, dvalues):
+        # Create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in \
+            enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output and
+            jacobian_matrix = np.diagflat(single_output) - \
+            np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
